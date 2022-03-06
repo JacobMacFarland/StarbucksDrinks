132eff9d19893f3375b8d7114fbb7296019a24ca
Jacob MacFarland <macfarlandjw@appstate.edu>
2022-03-06 13:41:53 -0600

    Add protocol for drink view controller delegate

---
+++ StarbucksDrinks/DrinkVCDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  DrinkVCDelegate.swift
+//  StarbucksDrinks
+//
+//  Created by Jacob MacFarland on 3/6/22.
+//
+
+import UIKit
+
+protocol DrinKVCDelegate: AnyObject {
+    func selectedCell(row: Int)
+}
